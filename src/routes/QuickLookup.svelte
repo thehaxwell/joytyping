089b226c6143ac55225f66827bbf356e14a468f8
Haxwell <thehaxwell@proton.me>
2023-08-07 22:36:41 +0200

    updated the QuickLookup ui code

--- src/routes/QuickLookup.svelte
+++ src/routes/QuickLookup.svelte
@@ -1,8 +1,12 @@
 <script>
   import AsciiControllerImage from "../lib/QuickLookup/AsciiControllerImage.svelte";
-let labels = ["␣","⌫","e","t","a","o","i","n","↵"," ","E","T","A","O","I","N"];
+let labels = {
+  l1s1: ["␣","⌫","e","t","a","o","i","n","↵"," ","E","T","A","O","I","N"],
+  l1s2: ["s","r","h","l","d","c","u","m","S","R","H","L","D","C","U","M"],
+  l1s3: ["f","p","g","w","y","b","v","k","F","P","G","W","Y","B","V","K"],
+  l1s4: ["x","j","q","z"," "," "," "," ","X","J","Q","Z"," "," "," "," "],
+};
 
 </script>
 
-<AsciiControllerImage {labels}/>
-
+<AsciiControllerImage labels={labels.l1s1}/>
