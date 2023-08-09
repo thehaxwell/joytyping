e472b2dfc1148d28d35c3bdcede759f7aeba48b4
Haxwell <thehaxwell@proton.me>
2023-08-09 18:36:24 +0200

    changed the quick-lookup keyboard layout

---
+++ src/lib/QuickLookup/CardinalButtonsCircle.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let buttons = [];
+  export let active = false;
+</script>
+
+<div class="circle flex-col justify-between p-2"
+  style={active ? "border-color: blue" : "border-color: black"}>
+  <div class="flex justify-center">
+    {buttons[0]}
+  </div>
+  <div class="flex justify-between items-center">
+    <div>{buttons[3]}</div>
+    <div>{buttons[1]}</div>
+  </div>
+  <div class="flex justify-center">
+    {buttons[2]}
+  </div>
+</div>
+
+<style>
+.circle {
+  height: 4.5rem;
+  width: 4.5rem;
+  border-width: 4px;
+  border-style: solid;
+  border-radius: 100%;
+}
+.flex-col {
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center;
+  align-items: center; */
+}
+.flex {
+  display: flex;
+}
+.justify-center{
+  justify-content: center;
+}
+.items-center{
+  align-items: center;
+}
+.justify-between{
+  justify-content: space-between;
+}
+.m-2 {
+  margin: 0.5rem;
+}
+.p-2 {
+  padding: 0.5rem;
+}
+</style>
--- src/routes/QuickLookup.svelte
+++ src/routes/QuickLookup.svelte
@@ -1,33 +1,19 @@
 <script>
-  import AsciiControllerImage from "../lib/QuickLookup/AsciiControllerImage.svelte";
   import { listen } from '@tauri-apps/api/event'
+  import Keyboard2 from "../lib/QuickLookup/Keyboard2.svelte";
 
   let stepNum = 1;
-listen('update-keyboard', (event) => {
-  console.log('==>',event.payload);
-  stepNum = event.payload.step;
- }).then();
+  let layerNum = 1;
+  listen('update-keyboard', (event) => {
+    console.log('==>',event.payload);
+    stepNum = event.payload.step;
+    layerNum = event.payload.layer;
+  }).then();
 //});
 </script>
 
-{#if stepNum == 1}
-step 1
-<AsciiControllerImage labels=
-                      {["␣","⌫","e","t","a","o","i","n","↵"," ","E","T","A","O","I","N"]}
-/>
-{:else if stepNum == 2}
-step 2
-<AsciiControllerImage labels=
-                      {["s","r","h","l","d","c","u","m","S","R","H","L","D","C","U","M"]}
-/>
-{:else if stepNum == 3}
-step 3
-<AsciiControllerImage labels=
-                      {["f","p","g","w","y","b","v","k","F","P","G","W","Y","B","V","K"]}
-/>
-{:else}
-step 4
-<AsciiControllerImage labels=
-                      {["x","j","q","z"," "," "," "," ","X","J","Q","Z"," "," "," "," "]}
-/>
-{/if}
+<!-- <AsciiControllerImage labels= -->
+<!--                       {["␣","⌫","e","t","a","o","i","n","↵"," ","E","T","A","O","I","N"]} -->
+<!-- /> -->
+
+<Keyboard2 {layerNum} {stepNum} />
