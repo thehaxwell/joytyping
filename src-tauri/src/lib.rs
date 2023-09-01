9d2d2a69ffcda688c6d63a86fc8340d6736986a3
Haxwell <thehaxwell@proton.me>
2023-09-01 13:33:59 +0200

    removed every module except settings and settings_data

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,8 @@
 use std::sync::mpsc;
 
-use joy_keyboard::input_controller::InputController;
-use joy_keyboard::keys_config::KeysConfig;
 use settings::error_display_window::ErrorDisplayWindow;
 use settings::{Settings,SettingsDependenciesImpl};
-use joy_keyboard::stepper::StepperButton;
-use gamepad::gilrs_wrapper::GilrsWrapper;
-use gamepad::sticks_interpreter::{SticksInterpreter, AxisClickThresholds};
-use joy_keyboard::input_controller::enigo_wrapper::EnigoWrapper;
-use quick_lookup_window::{QuickLookupWindow, QuickLookupWindowDependenciesImpl};
-    use gamepad::CustomButton;
 
-pub mod joy_keyboard;
-pub mod gamepad;
-pub mod quick_lookup_window;
 pub mod settings;
 pub mod settings_data;
 
@@ -21,10 +10,6 @@ pub fn start_main_loop(
     end_signal_mpsc_receiver: mpsc::Receiver<MainLoopInterruption>,
     handle: tauri::AppHandle
     ){
-    let mut quick_lookup_window = QuickLookupWindow::new(
-        handle.clone(),
-        Box::new(QuickLookupWindowDependenciesImpl),
-    );
     let mut settings_error_display_window = ErrorDisplayWindow::new(handle);
 
     'main_loop_initializer_loop: loop {
@@ -33,7 +18,7 @@ pub fn start_main_loop(
 
         let mut settings = Settings::new(
             Box::new(SettingsDependenciesImpl),
-            "/home/haxwell/.config/joytyping/joytyping.toml".to_string());
+            "/home/haxwell/.config/joytyping/joytyping1.toml".to_string());
 
         if let Err(e) = settings.load() {
            let _ = settings_error_display_window.open_and_show(e);
@@ -49,41 +34,6 @@ pub fn start_main_loop(
             None => 0
         });
 
-        let mut gamepad = gamepad::Gamepad::new(
-            Box::new(GilrsWrapper::new()),
-            Box::new(SticksInterpreter::new(
-            AxisClickThresholds::get_from_setting(
-                active_profile.left_stick.click_thresholds,
-                LeftOrRight::Left),
-            AxisClickThresholds::get_from_setting(
-                active_profile.right_stick.click_thresholds,
-                LeftOrRight::Right),
-                
-            )),
-        );
-        let mut joy_keyboard = joy_keyboard::JoyKeyboard::new(
-            Box::new(InputController::new(Box::new(EnigoWrapper::new()))),
-            Box::new(StepperButton::new()),
-            Box::new(StepperButton::new()),
-            KeysConfig::from(
-                active_profile.keyboard_mode.key_mappings)
-        );
-
-
-        quick_lookup_window.set_window_settings(active_profile.quick_lookup_window);
-        match quick_lookup_window.load_startup_script() {
-            Err(e) => {
-                match e {
-                    _ => {
-                        println!("Error!");
-                    }
-                }
-            },
-            Ok(_) => {
-                println!("quick lookup window external script");
-            }
-        }
-
         'main_loop: loop {
             //TODO: check if this actually eases the load on the CPU
             std::thread::sleep(std::time::Duration::from_millis(10));
@@ -97,64 +47,6 @@ pub fn start_main_loop(
                     break 'main_loop_initializer_loop;
                 }
                 Err(mpsc::TryRecvError::Empty) => {}
-            }
-
-            joy_keyboard.trigger_input();
-            while let Some(event) = gamepad.next_event() {
-                match event {
-                    gamepad::GamepadEvent::ButtonPressed(button)=> {
-                    print!("ButtonPressed: {:?}\n",button);
-                        match button{
-                            CustomButton::Base(gilrs::Button::LeftTrigger) => {
-                                joy_keyboard.set_l1_mod_is_down(true);
-                                let _ = quick_lookup_window.update_keyboard(
-                                    joy_keyboard.get_current_layer(),
-                                    joy_keyboard.get_current_step()
-                                );
-                            },
-                            CustomButton::Base(gilrs::Button::LeftTrigger2) => {
-                              // quick_lookup_window.open();
-                              let _ = quick_lookup_window.show_or_open();
-                            },
-                            CustomButton::Base(gilrs::Button::RightTrigger) => {
-                                joy_keyboard.set_r1_mod_is_down(true);
-                                let _ = quick_lookup_window.update_keyboard(
-                                    joy_keyboard.get_current_layer(),
-                                    joy_keyboard.get_current_step()
-                                );
-                            },
-                            _ => {
-                                joy_keyboard.button_pressed(button);
-                            },
-                        };
-
-                    },
-                    gamepad::GamepadEvent::ButtonReleased(button) => {
-                        print!("ButtonReleased: {:?}\n",button);
-                        match button {
-                            CustomButton::Base(gilrs::Button::LeftTrigger) => {
-                                joy_keyboard.set_l1_mod_is_down(false);
-                                let _ = quick_lookup_window.update_keyboard(
-                                    joy_keyboard.get_current_layer(),
-                                    joy_keyboard.get_current_step()
-                                );
-                            },
-                            CustomButton::Base(gilrs::Button::RightTrigger) => {
-                                joy_keyboard.set_r1_mod_is_down(false);
-                                let _ = quick_lookup_window.update_keyboard(
-                                    joy_keyboard.get_current_layer(),
-                                    joy_keyboard.get_current_step()
-                                );
-                            },
-                            CustomButton::Base(gilrs::Button::LeftTrigger2) => {
-                              let _ = quick_lookup_window.hide();
-                            },
-                            _ => {
-                                joy_keyboard.button_released();
-                            },
-                        }
-                    },
-                };
             }
 
         }
--- src-tauri/src/settings_data.rs
+++ src-tauri/src/settings_data.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug, Clone, PartialEq)]
+#[derive(Deserialize, Debug, Copy, Clone, PartialEq)]
 pub enum EnigoKey {
     Alt,
     Backspace,
@@ -177,4 +177,228 @@ pub struct Global {
 pub struct SettingsData {
 	pub profiles: Vec<Profile>,
     pub global: Global,
+}
+
+// NEW
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct SettingsData1 {
+	pub profiles: Vec<Profile1>,
+    pub global: Global,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Profile1 {
+	// pub keyboard_mode: KeyboardModeConfig,
+	pub name: String,
+    pub quick_lookup_window: QuickLookupWindow,
+    // pub left_stick: Axis,
+    // pub right_stick: Axis,
+    pub layers: Vec<Layer>,
+    pub stick_switches_click_thresholds: StickSwitchesClickThresholds,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct StickSwitchesClickThresholds {
+    pub left_stick_up: f32,
+    pub left_stick_down: f32,
+    pub left_stick_left: f32,
+    pub left_stick_right: f32,
+    pub right_stick_up: f32,
+    pub right_stick_down: f32,
+    pub right_stick_left: f32,
+    pub right_stick_right: f32,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Layer {
+    pub switches: Option<Switches>,
+    // pub levers
+    pub cardinal_levers: Option<CardinalLevers>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct CardinalLevers {
+    pub left_stick: CardinalLeverFunction,
+    pub right_stick: CardinalLeverFunction,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct CardinalLeverFunction {
+    pub control_mouse_cursor: Option<ControlMouseCursorFunction>,
+    pub control_mouse_scrollwheel: Option<ControlMouseScrollwheelFunction>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct ControlMouseCursorFunction {
+    center_at: ControlMouseCursorCenterCoordinates,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct ControlMouseCursorCenterCoordinates {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct ControlMouseScrollwheelFunction {
+    center_at_y: f32,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct Switches {
+	pub south: Option<SwitchEventAndReaction>,
+	pub east: Option<SwitchEventAndReaction>,
+	pub north: Option<SwitchEventAndReaction>,
+	pub west: Option<SwitchEventAndReaction>,
+	pub d_pad_up: Option<SwitchEventAndReaction>,
+	pub d_pad_down: Option<SwitchEventAndReaction>,
+	pub d_pad_left: Option<SwitchEventAndReaction>,
+	pub d_pad_right: Option<SwitchEventAndReaction>,
+	pub left_stick_up: Option<SwitchEventAndReaction>,
+	pub left_stick_down: Option<SwitchEventAndReaction>,
+	pub left_stick_left: Option<SwitchEventAndReaction>,
+	pub left_stick_right: Option<SwitchEventAndReaction>,
+	pub right_stick_up: Option<SwitchEventAndReaction>,
+	pub right_stick_down: Option<SwitchEventAndReaction>,
+	pub right_stick_left: Option<SwitchEventAndReaction>,
+	pub right_stick_right: Option<SwitchEventAndReaction>,
+	pub right_trigger: Option<SwitchEventAndReaction>,
+	pub left_trigger: Option<SwitchEventAndReaction>,
+
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct SwitchEventAndReaction {
+    on_click: Option<SwitchOnClickReaction>,
+    on_click_and_hold: Option<SwitchOnClickReaction>,
+    on_double_click: Option<SwitchOnClickReaction>,
+    on_double_click_and_hold: Option<SwitchOnClickReaction>,
+}
+
+#[derive(Deserialize, Debug, Clone, PartialEq)]
+pub struct SwitchOnClickReaction {
+    // first 3 taken from KeyClickConfig
+	pub key: Option<EnigoKey>,
+	pub modifiers: Option<Vec<EnigoKey>>,
+	pub char_key: Option<char>,
+
+    pub visit_layer: Option<String>,
+    pub move_to_layer: Option<String>,
+}
+
+impl SwitchOnClickReaction {
+    // If char_key is given, it takes precedence over key
+    pub fn get_enigo_key(&self) -> Option<enigo::Key> {
+        if let Some(key) = self.char_key {
+            Some(enigo::Key::Layout(key))
+        }
+        else if let Some(key) = self.key {
+            Some(SwitchOnClickReaction::to_enigo_key(key))
+        }
+        else {
+            None
+        }
+    }
+
+    pub fn get_enigo_modifiers(&self) -> Option<Vec<enigo::Key>> {
+        if let Some(mods) = &self.modifiers {
+            let keys = mods
+                .iter()
+                .map(|modifier|SwitchOnClickReaction::to_enigo_key(*modifier))
+                .collect();
+            Some(keys)
+        }
+        else {
+            None
+        }
+    }
+
+    fn to_enigo_key(key: EnigoKey) -> enigo::Key {
+        match key {
+            EnigoKey::Alt => enigo::Key::Alt,
+            EnigoKey::Backspace => enigo::Key::Backspace,
+            EnigoKey::Begin => enigo::Key::Begin,
+            EnigoKey::Break => enigo::Key::Break,
+            EnigoKey::Cancel => enigo::Key::Cancel,
+            EnigoKey::CapsLock => enigo::Key::CapsLock,
+            EnigoKey::Clear => enigo::Key::Clear,
+            EnigoKey::Control => enigo::Key::Control,
+            EnigoKey::Delete => enigo::Key::Delete,
+            EnigoKey::DownArrow => enigo::Key::DownArrow,
+            EnigoKey::End => enigo::Key::End,
+            EnigoKey::Escape => enigo::Key::Escape,
+            EnigoKey::Execute => enigo::Key::Execute,
+            EnigoKey::F1 => enigo::Key::F1,
+            EnigoKey::F2 => enigo::Key::F2,
+            EnigoKey::F3 => enigo::Key::F3,
+            EnigoKey::F4 => enigo::Key::F4,
+            EnigoKey::F5 => enigo::Key::F5,
+            EnigoKey::F6 => enigo::Key::F6,
+            EnigoKey::F7 => enigo::Key::F7,
+            EnigoKey::F8 => enigo::Key::F8,
+            EnigoKey::F9 => enigo::Key::F9,
+            EnigoKey::F10 => enigo::Key::F10,
+            EnigoKey::F11 => enigo::Key::F11,
+            EnigoKey::F12 => enigo::Key::F12,
+            EnigoKey::F13 => enigo::Key::F13,
+            EnigoKey::F14 => enigo::Key::F14,
+            EnigoKey::F15 => enigo::Key::F15,
+            EnigoKey::F16 => enigo::Key::F16,
+            EnigoKey::F17 => enigo::Key::F17,
+            EnigoKey::F18 => enigo::Key::F18,
+            EnigoKey::F19 => enigo::Key::F19,
+            EnigoKey::F20 => enigo::Key::F20,
+            EnigoKey::F21 => enigo::Key::F21,
+            EnigoKey::F22 => enigo::Key::F22,
+            EnigoKey::F23 => enigo::Key::F23,
+            EnigoKey::F24 => enigo::Key::F24,
+            EnigoKey::F25 => enigo::Key::F25,
+            EnigoKey::F26 => enigo::Key::F26,
+            EnigoKey::F27 => enigo::Key::F27,
+            EnigoKey::F28 => enigo::Key::F28,
+            EnigoKey::F29 => enigo::Key::F29,
+            EnigoKey::F30 => enigo::Key::F30,
+            EnigoKey::F31 => enigo::Key::F31,
+            EnigoKey::F32 => enigo::Key::F32,
+            EnigoKey::F33 => enigo::Key::F33,
+            EnigoKey::F34 => enigo::Key::F34,
+            EnigoKey::F35 => enigo::Key::F35,
+            EnigoKey::Find => enigo::Key::Find,
+            EnigoKey::Hangul => enigo::Key::Hangul,
+            EnigoKey::Hanja => enigo::Key::Hanja,
+            EnigoKey::Help => enigo::Key::Help,
+            EnigoKey::Home => enigo::Key::Home,
+            EnigoKey::Insert => enigo::Key::Insert,
+            EnigoKey::Kanji => enigo::Key::Kanji,
+            EnigoKey::LControl => enigo::Key::LControl,
+            EnigoKey::LeftArrow => enigo::Key::LeftArrow,
+            EnigoKey::Linefeed => enigo::Key::Linefeed,
+            EnigoKey::LMenu => enigo::Key::LMenu,
+            EnigoKey::LShift => enigo::Key::LShift,
+            EnigoKey::Meta => enigo::Key::Meta,
+            EnigoKey::ModeChange => enigo::Key::ModeChange,
+            EnigoKey::Numlock => enigo::Key::Numlock,
+            EnigoKey::Option => enigo::Key::Option,
+            EnigoKey::PageDown => enigo::Key::PageDown,
+            EnigoKey::PageUp => enigo::Key::PageUp,
+            EnigoKey::Pause => enigo::Key::Pause,
+            EnigoKey::Print => enigo::Key::Print,
+            EnigoKey::RControl => enigo::Key::RControl,
+            EnigoKey::Redo => enigo::Key::Redo,
+            EnigoKey::Return => enigo::Key::Return,
+            EnigoKey::RightArrow => enigo::Key::RightArrow,
+            EnigoKey::RShift => enigo::Key::RShift,
+            EnigoKey::ScrollLock => enigo::Key::ScrollLock,
+            EnigoKey::Select => enigo::Key::Select,
+            EnigoKey::ScriptSwitch => enigo::Key::ScriptSwitch,
+            EnigoKey::Shift => enigo::Key::Shift,
+            EnigoKey::ShiftLock => enigo::Key::ShiftLock,
+            EnigoKey::Space => enigo::Key::Space,
+            EnigoKey::SysReq => enigo::Key::SysReq,
+            EnigoKey::Tab => enigo::Key::Tab,
+            EnigoKey::UpArrow => enigo::Key::UpArrow,
+            EnigoKey::Undo => enigo::Key::Undo,
+        }
+    }
 }
