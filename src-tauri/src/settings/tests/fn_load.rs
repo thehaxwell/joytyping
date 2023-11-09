b99357c9b3c25b3994b9e46c4d4564fe54927ed2
Haxwell <thehaxwell@proton.me>
2023-11-09 18:02:16 +0200

    added settings module unit tests

--- src-tauri/src/settings/tests/fn_load.rs
+++ src-tauri/src/settings/tests/fn_load.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use mockall::predicate::*;
-use crate::{settings::{MockSettingsDependencies, Settings, SettingsLoadError}, app_data_directory_manager::MockAppDataDirectoryManagerTrait};
+use crate::{settings::{MockSettingsDependencies, Settings, SettingsLoadError, tests::settings_data_example::setup_settings_data_example_with_error}, app_data_directory_manager::MockAppDataDirectoryManagerTrait};
 
 use super::settings_data_example::setup_settings_data_example;
 
@@ -132,3 +132,74 @@ fn interprets_dependencies_read_to_string_error() {
     assert_eq!(settings.load(),Err(SettingsLoadError::FileNotReadable));
     assert!(settings.data.is_none());
 }
+
+// #[test]
+// fn interprets_dependencies_from_str_error() {
+//     let mut mock_app_data_directory_manager = MockAppDataDirectoryManagerTrait::new();
+//     let mut mock_deps = MockSettingsDependencies::new();
+//
+//     mock_app_data_directory_manager
+//         .expect_get_app_data_directory()
+//         .times(1)
+//         .return_const(Ok(Path::new("an/example/path").to_path_buf()));
+//
+//     mock_deps
+//         .expect_read_to_string()
+//         .times(1)
+//         .returning(|_|Ok("an example of the raw toml code".to_string()));
+//
+//     mock_deps
+//         .expect_from_str()
+//         .with(eq("an example of the raw toml code".to_string()))
+//         .times(1)
+//         .return_const(Err(toml::de::Error::message("something went wrong")));
+//
+//     let mut settings = Settings {
+//         data: None,
+//         dependencies: Box::new(mock_deps),
+//         app_data_directory_manager: Box::new(mock_app_data_directory_manager),
+//     };
+//
+//     assert_eq!(settings.load(),
+//         Err(SettingsLoadError::FileNotParsable("something went wrong".to_string())));
+//     assert!(settings.data.is_none());
+// }
+
+#[test]
+fn interprets_validate_and_clone_and_set_layer_pointers_error() {
+    let mut mock_app_data_directory_manager = MockAppDataDirectoryManagerTrait::new();
+    let mut mock_deps = MockSettingsDependencies::new();
+
+    mock_app_data_directory_manager
+        .expect_get_app_data_directory()
+        .times(1)
+        .return_const(Ok(Path::new("an/example/path").to_path_buf()));
+
+    mock_deps
+        .expect_read_to_string()
+        .times(1)
+        .returning(|_|Ok("an example of the raw toml code".to_string()));
+
+    mock_deps
+        .expect_from_str()
+        .with(eq("an example of the raw toml code".to_string()))
+        .times(1)
+        .return_const(Ok(setup_settings_data_example_with_error()));
+
+    let mut settings = Settings {
+        data: None,
+        dependencies: Box::new(mock_deps),
+        app_data_directory_manager: Box::new(mock_app_data_directory_manager),
+    };
+
+    assert_eq!(settings.load(),
+        Err(SettingsLoadError::FileNotParsable(format!("{}\n{}\n{}\n{}\n{}",
+            "Error in",
+            "> profiles: \"My PS3 Controller\"",
+            "   > stick_switches_click_thresholds",
+            "      > left_stick_left",
+            "value (1.5) is higher than the maximum acceptable 1.0",))));
+
+    assert!(settings.data.is_none());
+}
+
--- src-tauri/src/settings/tests/settings_data_example.rs
+++ src-tauri/src/settings/tests/settings_data_example.rs
@@ -42,3 +42,47 @@ pub fn setup_settings_data_example() -> SettingsData {
         development: None,
     }
 }
+
+
+pub fn setup_settings_data_example_with_error() -> SettingsData {
+    let ps4_controller_profile = Profile{
+        name: "My PS3 Controller".to_string(),
+        left_upper_is_d_pad: true,
+        quick_lookup_window: QuickLookupWindow{
+            theme: Some(QuickLookupWindowTheme::Light),
+            inner_size: HeightAndWidth{
+                height: 100.0,
+                width: 100.0,
+            },
+            source_code: BrowserSourceCode{
+                js_iife_bundle_file_path: "path/to/file/bundle.js".to_string(),
+                css_file_path: None,
+            }
+        },
+        stick_switches_click_thresholds: StickSwitchesClickThresholds {
+            left_stick_up: 0.5,
+            left_stick_down: 0.5,
+            left_stick_left: 1.5, // THE ERROR
+            left_stick_right: 0.5,
+            right_stick_up: 0.5,
+            right_stick_down: 0.5,
+            right_stick_left: 0.5,
+            right_stick_right: 0.5
+        },
+        trigger_2_switches_click_thresholds: Trigger2SwitchesClickThresholds { 
+            left_trigger_2: 0.3,
+            right_trigger_2: 0.3,
+        },
+        switch_click_event_thresholds: SwitchClickEventThresholds {
+            minimum_milliseconds_down_for_click_and_hold: 500,
+            maximum_milliseconds_between_clicks_for_double_click: 500,
+        },
+        layers: Vec::new()
+    };
+
+    SettingsData {
+        profiles: vec![ps4_controller_profile],
+        global: Global{default_profile:"My PS3 Controller".to_string()},
+        development: None,
+    }
+}
